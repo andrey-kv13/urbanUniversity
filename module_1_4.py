d6a8f836fb3bd012ee682a2b0f9c6a9c98a75197
an.kurbatov <andreyfbinst@gmail.com>
2024-12-15 14:05:46 +0300

    HomeWork for module0

---
+++ module_1_4.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    my_string = input("Введите текст произвольной строки:")
+    print(my_string.upper(), my_string.lower())
+    print(my_string.replace(" ", ""))
+    print(my_string[0], my_string[-1])
